137792af76a86e1bbc644ab02f0ee0c15871f462
Anna Harrison <annaharri89@gmail.com>
2017-07-13 13:54:31 -0400

    Added readme.

--- app/src/main/java/com/example/etaspare/stitchcounter/LibraryActivity.java
+++ app/src/main/java/com/example/etaspare/stitchcounter/LibraryActivity.java
@@ -34,19 +34,6 @@ import java.util.ArrayList;
 
 public class LibraryActivity extends AppCompatActivity
         implements LoaderManager.LoaderCallbacks<Cursor> {
-    /* TODO: Implement help button */
-    /* TODO: implement layouts for different sized devices */
-    /* TODO: Remove unneeded import statements throughout the app */
-    /* TODO: have a textview that says "You have no saved projects" when there are no saved projects */
-    /* TODO: REMOVE SUPERFLOUS CODE/COMMENTS */
-    /* TODO: LOOK INTO LOAD BALANCERS */
-    /* TODO: implement add image(s) to counter project */
-    /* TODO: Implement share project to facebook, twitter, etc. */
-    /*TODO Provide thorough documentation throughout app*/
-    /* TODO FIgure out why changing a counter, then going to library, then loading the same counter, results in the counter not being updated, look into adapter.notifyDataSetChanged(); */
-    /* TODO Figure out why changing a double counter's progress, hitting the back button, results in list item's progress not updating */
-    /* TODO Figure out why clicking into a counter, hitting the back button, results in first item being displayed as both the first item and the last item in the listview */
-    /* TODO fix issue: app crashes when you try to start a double counter without putting total rows */
 
     final private StitchCounterMenu toolBarMenu = new StitchCounterMenu(this);
     ConstraintLayout topLayout;
